65689df7b4b998e397bf574d0d374a73b95578dd
andra <andras_kapitany@hotmail.hu>
2023-06-10 16:06:43 +0200

    super.toString added

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ import data.Student;
 public class Main {
     public static void main(String[] args) {
         // Ezt nyugodtan módosítsd!
-        Person person = new Person();
-        Staff staff = new Staff();
-        Student student = new Student();
+        Person person = new Person("Lali","1154 BP Kl");
+        Staff staff = new Staff("Liza","1134 BP LK","Kolozsvár",100000);
+        Student student = new Student("Gizi","1120 BP IKJ","BACKEND",2023,20140);
+
+        System.out.println(person);
     }
 }
